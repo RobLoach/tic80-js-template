ca4f5083ba16c5254454626f65728fa8624e07cf
Rob Loach <robloach@gmail.com>
2019-06-10 16:24:13 -0400

    Update documentation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // title:  TIC-80 JS Template
+// desc:   Demonstration of an ES6 JavaScript TIC-80 game. See more at https://github.com/RobLoach/tic80-js-template .
 // author: Rob Loach
-// desc:   Use this template to build ES6 JavaScript TIC-80 games.
 // script: js
 
 let t = 0
@@ -9,13 +9,17 @@ let y = 24
 let world = 'WORLD'
 
 export function TIC() {
+	// Input
 	if (btn(0)) y--
 	if (btn(1)) y++
 	if (btn(2)) x--
 	if (btn(3)) x++
 
+	// Draw
 	cls(13)
 	spr(1 + ( (t % 60) / 30 | 0) * 2, x, y, 14, 3, 0, 0, 2, 2)
 	print(`HELLO ${world}!`, 84, 84)
+
+	// Timer
 	t++
 }
